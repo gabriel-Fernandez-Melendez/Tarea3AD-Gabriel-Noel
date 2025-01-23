3804c29b2b1b9f78bd2799293d01c7b280b3eb82
unknown <yic00732@educastur.es>
2025-01-23 16:49:08 +0100

    commit de los cambios a la ventana

--- src/main/java/com/Gabriel/Noel/tarea3AD2024base/controller/NuevoPeregrinoController.java
+++ src/main/java/com/Gabriel/Noel/tarea3AD2024base/controller/NuevoPeregrinoController.java
@@ -5,6 +5,8 @@ import java.util.ResourceBundle;
 
 import org.springframework.stereotype.Controller;
 
+import com.Gabriel.Noel.tarea3AD2024base.modelo.Parada;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -22,97 +24,94 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 @Controller
-public class NuevoPeregrinoController implements Initializable{
-
-    @FXML
-    private PasswordField Contraseña;
-
-    @FXML
-    private TextField Nombre_login;
-
-    @FXML
-    private ImageView imagen;
-
-    @FXML
-    private Label mostrar_contraseña;
-
-    @FXML
-    private TextField nombre_peregrino;
-
-    @FXML
-    private ChoiceBox<?> pais;
-
-    @FXML
-    private ComboBox<?> parada;
-    
-
+public class NuevoPeregrinoController implements Initializable {
 
 	@FXML
-	public void AyudaJavaFX( ) {
+	private PasswordField Contraseña;
+
+	@FXML
+	private TextField Nombre_login;
+
+	@FXML
+	private ImageView imagen;
+
+	@FXML
+	private Label mostrar_contraseña;
+
+	@FXML
+	private TextField nombre_peregrino;
+
+	@FXML
+	private ChoiceBox<String> pais;
+
+	@FXML
+	private ChoiceBox<Parada> parada;
+
+	@FXML
+	public void AyudaJavaFX() {
 		try {
 			System.out.println("entro a la funcion de ayuda pero no cargo la ventana");
-		    // Crear un WebView para mostrar la ayuda
-		    WebView webView = new WebView();
+			// Crear un WebView para mostrar la ayuda
+			WebView webView = new WebView();
 
-		    // Cargar el archivo HTML desde los recursos
-		    String url = getClass().getResource("/ayuda/Ayuda.html").toExternalForm();
-		    webView.getEngine().load(url);
+			// Cargar el archivo HTML desde los recursos
+			String url = getClass().getResource("/ayuda/Ayuda.html").toExternalForm();
+			webView.getEngine().load(url);
 
-		    // Crear un nuevo Stage para la ventana de ayuda
-		    Stage helpStage = new Stage();
-		    helpStage.setTitle("Ayuda");
+			// Crear un nuevo Stage para la ventana de ayuda
+			Stage helpStage = new Stage();
+			helpStage.setTitle("Ayuda");
 
-		    // Crear una Scene con el WebView
-		    Scene helpScene = new Scene(webView, 600, 600);
+			// Crear una Scene con el WebView
+			Scene helpScene = new Scene(webView, 600, 600);
 
-		    // Configurar la ventana
-		    helpStage.setScene(helpScene);
+			// Configurar la ventana
+			helpStage.setScene(helpScene);
 
-		    // Bloquea la ventana principal mientras se muestra la ayuda
-		    helpStage.initModality(Modality.APPLICATION_MODAL);
-		    helpStage.setResizable(true);
+			// Bloquea la ventana principal mientras se muestra la ayuda
+			helpStage.initModality(Modality.APPLICATION_MODAL);
+			helpStage.setResizable(true);
 
-		    // Mostrar la ventana de ayuda
-		    helpStage.show();
+			// Mostrar la ventana de ayuda
+			helpStage.show();
 
 		} catch (NullPointerException e) {
-		    // Manejar el caso en que el archivo de ayuda no se encuentra
-		    Alert alert = new Alert(Alert.AlertType.ERROR);
-		    alert.setTitle("Error");
-		    alert.setHeaderText("Archivo de Ayuda no encontrado");
-		    alert.setContentText("Por favor, verifica que el archivo 'help.html' esté en la ruta '/ayuda/help.html'.");
-		    alert.showAndWait();
-		    //esta linea en caso de que necesitemos detectar el origen del fallo atravez de consola
-		    e.printStackTrace();
-		    }
+			// Manejar el caso en que el archivo de ayuda no se encuentra
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText("Archivo de Ayuda no encontrado");
+			alert.setContentText("Por favor, verifica que el archivo 'help.html' esté en la ruta '/ayuda/help.html'.");
+			alert.showAndWait();
+			// esta linea en caso de que necesitemos detectar el origen del fallo atravez de
+			// consola
+			e.printStackTrace();
 		}
-	
-	
+	}
+
 	@FXML
 	private void ImagenPrecionada() {
-		//imagen.setImage(img);
+		// imagen.setImage(img);
 		System.out.println("entra al metodo de precionado");
-		Image img=new Image(getClass().getResourceAsStream("/images/eye.png"));
+		Image img = new Image(getClass().getResourceAsStream("/images/eye.png"));
 		imagen.setImage(img);
 		System.out.println("cargo");
-		mostrar_contraseña.setText("Tu contraseña es: "+Contraseña.getText());
+		mostrar_contraseña.setText("Tu contraseña es: " + Contraseña.getText());
 		mostrar_contraseña.setVisible(true);
 	}
+
 	@FXML
 	private void Imagensoltada() {
-		Image img=new Image(getClass().getResourceAsStream("/images/hide.png"));
+		Image img = new Image(getClass().getResourceAsStream("/images/hide.png"));
 		imagen.setImage(img);
 		System.out.println("entra al metodo de soltado");
 		mostrar_contraseña.setVisible(false);
-		
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		mostrar_contraseña.setVisible(false);
-		
+
 	}
 
 }
-
