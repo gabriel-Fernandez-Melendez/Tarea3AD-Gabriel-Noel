a988e035fd8694e8641b7cadafdef50ef29324dc
alumnoDAM <yic00732@educastur.es>
2025-01-31 21:09:29 +0100

    final

--- src/main/java/com/Gabriel/Noel/tarea3AD2024base/controller/NuevoPeregrinoController.java
+++ src/main/java/com/Gabriel/Noel/tarea3AD2024base/controller/NuevoPeregrinoController.java
@@ -193,11 +193,9 @@ public class NuevoPeregrinoController implements Initializable {
 		if (Contraseña.getText().contains(" ")) {
 			mostrarAlerta("contraseña no valida", "no puede tener espacios blancos la contraseña", AlertType.ERROR);
 		} 
-		if (!correo_peregrino.getText().matches("^[\\w-+]+(\\.[\\w-]{1,62}){0,126}@[\\w-]{1,63}(\\.[\\w-]{1,62})+/[\\w-]+$")) {
+		//exprecion regular saca de stack over flow https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
+		if (!correo_peregrino.getText().matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
 			mostrarAlerta("contraseña no valida", "falta el @", AlertType.ERROR);
-		}
-		if (!correo_peregrino.getText().contains(".com")||!correo_peregrino.getText().contains(".es")) {
-			mostrarAlerta("contraseña no valida", "falta el .com", AlertType.ERROR);
 		}
 		else {
 			Credenciales cred = GuardarNuevasCredenciales();
