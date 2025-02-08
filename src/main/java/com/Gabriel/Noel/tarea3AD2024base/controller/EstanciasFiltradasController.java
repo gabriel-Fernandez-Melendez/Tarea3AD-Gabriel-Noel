453b6685e98eba88a6fbff480827597088b6471b
alumnoDAM <yic00732@educastur.es>
2025-02-08 18:38:24 +0100

    implementacion de los textos de ayuda

--- src/main/java/com/Gabriel/Noel/tarea3AD2024base/controller/EstanciasFiltradasController.java
+++ src/main/java/com/Gabriel/Noel/tarea3AD2024base/controller/EstanciasFiltradasController.java
@@ -3,6 +3,7 @@ package com.Gabriel.Noel.tarea3AD2024base.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -13,6 +14,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.web.WebView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -115,7 +119,46 @@ public class EstanciasFiltradasController {
 		colSeEstancio.setCellValueFactory(new PropertyValueFactory<>("seEstancio"));
 		colEsVIP.setCellValueFactory(new PropertyValueFactory<>("esVIP"));
 	}
-	
+	@FXML
+	public void AyudaJavaFX() {
+		try {
+			System.out.println("entro a la funcion de ayuda pero no cargo la ventana");
+			// Crear un WebView para mostrar la ayuda
+			WebView webView = new WebView();
+
+			// Cargar el archivo HTML desde los recursos
+			String url = getClass().getResource("/ayuda/Ayuda.html").toExternalForm();
+			webView.getEngine().load(url);
+
+			// Crear un nuevo Stage para la ventana de ayuda
+			Stage helpStage = new Stage();
+			helpStage.setTitle("Ayuda");
+
+			// Crear una Scene con el WebView
+			Scene helpScene = new Scene(webView, 600, 600);
+
+			// Configurar la ventana
+			helpStage.setScene(helpScene);
+
+			// Bloquea la ventana principal mientras se muestra la ayuda
+			helpStage.initModality(Modality.APPLICATION_MODAL);
+			helpStage.setResizable(true);
+
+			// Mostrar la ventana de ayuda
+			helpStage.show();
+
+		} catch (NullPointerException e) {
+			// Manejar el caso en que el archivo de ayuda no se encuentra
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Error");
+			alert.setHeaderText("Archivo de Ayuda no encontrado");
+			alert.setContentText("Por favor, verifica que el archivo 'help.html' esté en la ruta '/ayuda/help.html'.");
+			alert.showAndWait();
+			// esta linea en caso de que necesitemos detectar el origen del fallo atravez de
+			// consola
+			e.printStackTrace();
+		}
+	}
 	@FXML
 	private void filtrarEstancias() {
 	    try {
