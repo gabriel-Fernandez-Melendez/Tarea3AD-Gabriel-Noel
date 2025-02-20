0b1699907f906319ce23648173e812b5fe04fb4e
noelRDB <noelgijon98@gmail.com>
2025-02-20 19:22:58 +0100

    Rezando

--- src/main/java/com/Gabriel/Noel/tarea3AD2024base/controller/EstanciasFiltradasController.java
+++ src/main/java/com/Gabriel/Noel/tarea3AD2024base/controller/EstanciasFiltradasController.java
@@ -21,6 +21,7 @@ import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
+
 import com.Gabriel.Noel.tarea3AD2024base.config.StageManager;
 import com.Gabriel.Noel.tarea3AD2024base.modelo.Credenciales;
 import com.Gabriel.Noel.tarea3AD2024base.modelo.Parada;
@@ -28,8 +29,6 @@ import com.Gabriel.Noel.tarea3AD2024base.modelo.PeregrinoTabla;
 import com.Gabriel.Noel.tarea3AD2024base.services.EstanciaService;
 import com.Gabriel.Noel.tarea3AD2024base.services.ParadaService;
 import com.Gabriel.Noel.tarea3AD2024base.view.FxmlView;
-
-
 
 import java.time.LocalDate;
 import java.util.List;
@@ -168,46 +167,7 @@ public class EstanciasFiltradasController {
 		colSeEstancio.setCellValueFactory(new PropertyValueFactory<>("seEstancio"));
 		colEsVIP.setCellValueFactory(new PropertyValueFactory<>("esVIP"));
 	}
-	@FXML
-	public void AyudaJavaFX() {
-		try {
-			System.out.println("entro a la funcion de ayuda pero no cargo la ventana");
-			// Crear un WebView para mostrar la ayuda
-			WebView webView = new WebView();
-
-			// Cargar el archivo HTML desde los recursos
-			String url = getClass().getResource("/ayuda/Ayuda.html").toExternalForm();
-			webView.getEngine().load(url);
-
-			// Crear un nuevo Stage para la ventana de ayuda
-			Stage helpStage = new Stage();
-			helpStage.setTitle("Ayuda");
-
-			// Crear una Scene con el WebView
-			Scene helpScene = new Scene(webView, 600, 600);
-
-			// Configurar la ventana
-			helpStage.setScene(helpScene);
-
-			// Bloquea la ventana principal mientras se muestra la ayuda
-			helpStage.initModality(Modality.APPLICATION_MODAL);
-			helpStage.setResizable(true);
-
-			// Mostrar la ventana de ayuda
-			helpStage.show();
-
-		} catch (NullPointerException e) {
-			// Manejar el caso en que el archivo de ayuda no se encuentra
-			Alert alert = new Alert(Alert.AlertType.ERROR);
-			alert.setTitle("Error");
-			alert.setHeaderText("Archivo de Ayuda no encontrado");
-			alert.setContentText("Por favor, verifica que el archivo 'help.html' esté en la ruta '/ayuda/help.html'.");
-			alert.showAndWait();
-			// esta linea en caso de que necesitemos detectar el origen del fallo atravez de
-			// consola
-			e.printStackTrace();
-		}
-	}
+	
 	@FXML
 	private void filtrarEstancias() {
 	    try {
