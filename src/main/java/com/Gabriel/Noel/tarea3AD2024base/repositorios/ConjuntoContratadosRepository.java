ba846b3bada6a9c630b6734beb3f62eb639dd60b
unknown <yic00732@educastur.es>
2025-02-20 13:14:00 +0100

    fallo en el repository

--- src/main/java/com/Gabriel/Noel/tarea3AD2024base/repositorios/ConjuntoContratadosRepository.java
+++ src/main/java/com/Gabriel/Noel/tarea3AD2024base/repositorios/ConjuntoContratadosRepository.java
@@ -1,22 +1,22 @@
 package com.Gabriel.Noel.tarea3AD2024base.repositorios;
 
+
+
+import org.springframework.stereotype.Repository;
+
 import com.Gabriel.Noel.tarea3AD2024base.modelo.ConjuntoContratado;
-import com.Gabriel.Noel.tarea3AD2024base.modelo.Servicio;
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
 
+@Repository
 public class ConjuntoContratadosRepository {
-	 //habia que que quitar la palabra final del atrubito para que cerrar la conexion en una operacion no lo haga de forma permanente
-	private ObjectContainer db = Db4oEmbedded.openFile("DB4O_Peregrinos.db4o");
-	
-	public void guardarServicio(ConjuntoContratado c) {
-        try {
-            db.store(c);
-            db.commit();
-            System.out.println("Servicio guardado en DB4O: " + c); //mensaje solo para el usuario
-        } catch (Exception e) {
-            System.out.println("Error al guardar el servicio: " + e.getMessage());
-        }
-    }
-	
+	/*
+	 * private final ObjectContainer db =
+	 * Db4oEmbedded.openFile("DB4O_Peregrinos.db4o");
+	 * 
+	 * public void guardarServicio(ConjuntoContratado c) { try { db.store(c);
+	 * db.commit(); System.out.println("Servicio guardado en DB4O: " + c); } catch
+	 * (Exception e) { System.out.println("Error al guardar el servicio: " +
+	 * e.getMessage()); } }
+	 */
 }
--- src/main/java/com/Gabriel/Noel/tarea3AD2024base/services/ConjuntoContradadoService.java
+++ src/main/java/com/Gabriel/Noel/tarea3AD2024base/services/ConjuntoContradadoService.java
@@ -1,19 +1,18 @@
 package com.Gabriel.Noel.tarea3AD2024base.services;
 
+
 import org.springframework.stereotype.Service;
 
 import com.Gabriel.Noel.tarea3AD2024base.modelo.ConjuntoContratado;
 import com.Gabriel.Noel.tarea3AD2024base.repositorios.ConjuntoContratadosRepository;
 
-@Service // creo que no hace falta notar las clases
-public class ConjuntoContradadoService {
-	private ConjuntoContratadosRepository servicio_contratado;
 
-	public ConjuntoContradadoService(ConjuntoContratadosRepository servicio_contratado) {
-		this.servicio_contratado = servicio_contratado;
-	}
-	
-	public void GuardarConjunto(ConjuntoContratado c) {
-		servicio_contratado.guardarServicio(c);
-	}
+@Service
+public class ConjuntoContradadoService {
+	/*
+	 * private ConjuntoContratadosRepository conjunto_rep;
+	 * 
+	 * public void GuardarConjunto(ConjuntoContratado c) {
+	 *conjunto_rep.guardarServicio(c); }
+	 */
 }
