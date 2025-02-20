c903920b561ea2b1a06f7c91cde91844767f9438
unknown <yic00732@educastur.es>
2025-02-20 11:53:28 +0100

    persistencia DB4O

--- src/main/java/com/Gabriel/Noel/tarea3AD2024base/services/ConjuntoContradadoService.java
+++ src/main/java/com/Gabriel/Noel/tarea3AD2024base/services/ConjuntoContradadoService.java
@@ -1,5 +1,19 @@
 package com.Gabriel.Noel.tarea3AD2024base.services;
 
+import org.springframework.stereotype.Service;
+
+import com.Gabriel.Noel.tarea3AD2024base.modelo.ConjuntoContratado;
+import com.Gabriel.Noel.tarea3AD2024base.repositorios.ConjuntoContratadosRepository;
+
+@Service // creo que no hace falta notar las clases
 public class ConjuntoContradadoService {
-//aqui hacemos la llamada a los metodos de lo anterior
+	private ConjuntoContratadosRepository servicio_contratado;
+
+	public ConjuntoContradadoService(ConjuntoContratadosRepository servicio_contratado) {
+		this.servicio_contratado = servicio_contratado;
+	}
+	
+	public void GuardarConjunto(ConjuntoContratado c) {
+		servicio_contratado.guardarServicio(c);
+	}
 }
