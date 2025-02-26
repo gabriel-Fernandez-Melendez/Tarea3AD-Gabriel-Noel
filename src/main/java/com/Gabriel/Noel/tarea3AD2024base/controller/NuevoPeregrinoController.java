afecb3304d793ab49949be918d1ae16554da4891
alumnoDAM <yic00732@educastur.es>
2025-02-26 16:58:55 +0100

    cambios en las validaciones de un nuevo peregrino

--- src/main/java/com/Gabriel/Noel/tarea3AD2024base/controller/NuevoPeregrinoController.java
+++ src/main/java/com/Gabriel/Noel/tarea3AD2024base/controller/NuevoPeregrinoController.java
@@ -184,25 +184,29 @@ public class NuevoPeregrinoController implements Initializable {
 	// TENGO QUE METER EL METOD FXML RELATIVO AL BOTON QUE GUARDA CASA COSA(actualmente implementado)
 	@FXML
 	private void GuardarNuevoPeregrino() {
-		if (Nombre_login.getText().contains(" ")) {
+		if (Nombre_login.getText().isEmpty()) {
 			mostrarAlerta("Nombre no valido", "no puede tener espacios blancos el nombre", AlertType.ERROR);
+			return;
 		}
-		if (Contraseña.getText().contains(" ")) {
+		if (Contraseña.getText().isEmpty()) {
 			mostrarAlerta("contraseña no valida", "no puede tener espacios blancos la contraseña", AlertType.ERROR);
+			return;
 		} 
 		//exprecion regular saca de stack over flow https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
 		if (!correo_peregrino.getText().matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
 			mostrarAlerta("contraseña no valida", "falta el @", AlertType.ERROR);
+			if(nombre_peregrino.getText().isEmpty()) {
+				mostrarAlerta("contraseña no valida", "falta el @", AlertType.ERROR);
+			}
+				
+			return;
 		}
-		else {
 			Credenciales cred = GuardarNuevasCredenciales();
 			Carnet c = GuardarCarnet(parada.getValue());
 			Carnet carnet_aux = carnet_service.BuscarPorId(c.getId());
 			GuardarPeregrinoSellado(cred, carnet_aux);
 			mostrarAlerta("Peregrino añadido", "Puede entrar con sus credenciales de peregrino", AlertType.INFORMATION);
 			stageManager.switchScene(FxmlView.LOGIN);// que una vez registrado con exito te lleve directamente a el login
-		}
-
 	}
 
 	// aqui iran los metodos que se implementen en los @fxml pero no tengan que
