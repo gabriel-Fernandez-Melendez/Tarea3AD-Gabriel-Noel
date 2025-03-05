b3f05bb9f443386149f4d2275dd7e5e969541ffa
alumnoDAM <yic00732@educastur.es>
2025-03-05 16:27:40 +0100

    creacion de las clases de testing

---
+++ src/test/java/com/luisdbb/tarea3AD2024base/CredencialesControllerTest.java
@@ -0,0 +1,5 @@
+package com.luisdbb.tarea3AD2024base;
+
+public class CredencialesControllerTest {
+
+}
--- src/test/java/com/luisdbb/tarea3AD2024base/Tarea3Ad2024baseApplicationTests.java
+++ src/test/java/com/luisdbb/tarea3AD2024base/Tarea3Ad2024baseApplicationTests.java
@@ -3,6 +3,8 @@ package com.luisdbb.tarea3AD2024base;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+//esta clase de momento no la voy a itilizar pero hay que utilizar el mock para precargar los campos del fxml 
 @SpringBootTest
 class Tarea3Ad2024baseApplicationTests {
 
